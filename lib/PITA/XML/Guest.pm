e9793fe7241081cc2386b07fecdb55805090c689
Adam Kennedy <adamk@cpan.org>
2006-06-17 14:15:42 +0000

    Import of ADAMK/PITA-XML-0.30 from CPAN.
    gitpan-cpan-distribution: PITA-XML
    gitpan-cpan-version:      0.30
    gitpan-cpan-path:         ADAMK/PITA-XML-0.30.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/PITA/XML/Guest.pm
+++ lib/PITA/XML/Guest.pm
@@ -28,16 +28,17 @@ represented in L<PITA::XML> by L<PITA::XML::Platform> objects.
 
 use strict;
 use base 'PITA::XML::Storable';
-use Carp         ();
-use Params::Util '_INSTANCE',
-                 '_STRING',
-                 '_CLASS',
-                 '_HASH0',
-                 '_SET0';
+use Carp             ();
+use Class::Inspector ();
+use Params::Util     '_INSTANCE',
+                     '_STRING',
+                     '_CLASS',
+                     '_HASH0',
+                     '_SET0';
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.29';
+	$VERSION = '0.30';
 }
 
 sub xml_entity { 'guest' }
@@ -53,6 +54,37 @@ my %ALLOWED = (
 	driver   => 1,
 	config   => 1,
 	);
+
+=pod
+
+=head2 new
+
+  # The most correct way to specify a guest
+  my $guest1 = PITA::XML::Guest->new(
+      driver => 'Qemu',
+      config => {
+          memory   => 256,
+          snapshot => 1,
+      }
+  );
+  
+  # Equivalent, using shorthand.
+  # Anything other that 'driver' is considered a config entry.
+  my $guest = PITA::XML::Guest->new(
+      driver   => 'Qemu',
+      memory   => 256,
+      snapshot => 1,
+      );
+
+The C<new> constructor creates a new E<lt>guestE<gt> element.
+
+It has a single compulsory parameter of the guest driver name, and takes
+optionally a set of named params to provide as creation params for the
+guest driver object.
+
+Returns a new L<PITA::XML::Guest> or throw an exception on error.
+
+=cut
 
 sub new {
 	my $class  = shift;
@@ -75,7 +107,18 @@ sub new {
 	$self;
 }
 
-# ugh... ugly duplication, but will do for now
+=pod
+
+=head2 read
+
+  $guest = PITA::XML::Guest->new( 'guest.xml' );
+
+The C<read> constructor loads a guest from an existing L<PITA::XML> file.
+
+Returns a new L<PITA::XML::Guest> object, or throws an exception on error.
+
+=cut
+
 sub read {
 	my $class = shift;
 	my $fh    = PITA::XML->_FH(shift);
@@ -126,13 +169,109 @@ sub _init {
 	$self;
 }
 
+=pod
+
+=head2 driver
+
+The C<driver> accessor returns the shorthand name of the driver, as it
+is stored in the PITA-XML xml file.
+
+For example, if the guest uses the L<PITA::Guest::Driver::Qemu> driver,
+the C<driver> method return C<'Qemu'>.
+
+=cut
+
 sub driver {
 	$_[0]->{driver};
 }
 
+=pod
+
+=head2 driver_available
+
+The C<driver_available> method will check your local system to see if the
+driver for this guest is available in the current Perl environment.
+
+Returns true if the driver is available, or false if not.
+
+=cut
+
+sub driver_available {
+	my $self   = shift;
+	my $driver = 'PITA::Guest::' . $self->driver;
+	Class::Inspector->available( $driver );
+}
+
+=pod
+
+=head2 config
+
+The C<config> accessor returns the configuration for the driver.
+
+This configuration is entirely driver-specific, and although conventions
+may exist, you should not rely on the contents of the configuration to
+have any specific meaning.
+
+Returns a reference to a C<HASH> containing plain scalar keys and values.
+
+=cut
+
 sub config {
 	$_[0]->{config};
 }
+
+=pod
+
+=head2 files
+
+Each guest will require zero or more file resources. In most cases, this
+consists of drive images or emulator configuration files.
+
+The C<files> method returns all existing files for this guest.
+
+Returns a list of L<PITA::XML::File> objects
+
+=cut
+
+sub files {
+	@{ $_[0]->{files} };
+}
+
+=pod
+
+=head2 platforms
+
+Each guest should contain one or more testing contexts, where packages
+of some specific type can be automatically tested. In PITA parlance, a
+scheme-specific testing context is known as a I<Platform>.
+
+The C<platforms> method returns all existing known platforms for this
+guest.
+
+Returns a list of one of more L<PITA::XML::Platform> objects.
+
+If this method returns a zero-length list, then the guest may be
+unprocessed, and has not been 'discovered' yet.
+
+=cut
+
+sub platforms {
+	@{ $_[0]->{platforms} };
+}
+
+=pod
+
+=head2 add_file
+
+The C<add_file> method adds a new driver-specific file to
+the guest.
+
+It takes as it's only parameter a L<PITA::XML::File> object.
+
+Returns true if added, or throws an exception if not passed a valid
+L<PITA::XML::File> object.
+
+=cut
 
 sub add_file {
 	my $self = shift;
@@ -146,9 +285,26 @@ sub add_file {
 	1;
 }
 
-sub files {
-	@{ $_[0]->{files} };
-}
+=pod
+
+=head2 add_platform
+
+The C<add_platform> method adds a new testing context to the guest.
+
+In general, you should B<not> be manually adding platform definitions
+to the guest unless you are implementing a driver auto-discovery
+mechanism for your new or custom L<PITA::Guest::Driver> class.
+
+That is, the PITA driver system itself will take you unprocessed guest,
+load it, query the guest for its platform list, and update the XML file
+independantly, without the help of any external system.
+
+It takes as it's only parameter a L<PITA::XML::Platform> object.
+
+Returns true if added, or throws an exception if not passed a valid
+L<PITA::XML::File> object.
+
+=cut
 
 sub add_platform {
 	my $self     = shift;
@@ -162,16 +318,23 @@ sub add_platform {
 	1;
 }
 
-sub platforms {
-	@{ $_[0]->{platforms} };
-}
-
 
 
 
 
 #####################################################################
 # Main Methods
+
+=pod
+
+=head2 discovered
+
+The C<discovered> method is a convenience method, and checks to see if
+platform discovery has been done on the guest, or if it is unprocessed.
+
+Returns true if the platforms have been discovered, or false if not.
+
+=cut
 
 sub discovered {
 	!! $_[0]->platforms;
@@ -197,7 +360,7 @@ Adam Kennedy E<lt>cpan@ali.asE<gt>, L<http://ali.as/>
 
 L<PITA::XML>
 
-The Perl Image-based Testing Architecture (L<http://ali.as/pita/>)
+The Perl Image Testing Architecture (L<http://ali.as/pita/>)
 
 =head1 COPYRIGHT
 
